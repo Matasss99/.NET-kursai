27c524b14f3d2c0eb64d2e0b55b372b610c26c91
Matas Jonaitis <matas_jonaitis@codeacademylt.onmicrosoft.com>
2022-06-07 20:26:42 +0300

    console uzduotys

--- Basic Mokymai/Basic Mokymai/Program.cs
+++ Basic Mokymai/Basic Mokymai/Program.cs
@@ -29,8 +29,7 @@
             Console.WriteLine($"o štai mano pakartojimas {Console.ReadLine()}" );
 
             Console.WriteLine("Įveskite raidę");
-            var key = Console.ReadKey();
-            Console.WriteLine("Įvestas simbolis {0}", key.KeyChar);
+            Console.WriteLine("Įvestas simbolis {0}", Console.Readkey().KeyChar);
 
             Console.WriteLine("Įveskite savo vardą, o aš atspėsiu pirmą raidę:");
             Console.WriteLine("o štai mano spėjimas \"" + Console.ReadLine()[0] + "\"");
@@ -44,20 +43,48 @@
             Console.WriteLine(@" "" tekstas kabutėse ""
             eilutė 1
             eilutė 2
-            eilutė 3"); */
+            eilutė 3"); 
 
-            Console.WriteLine("Mano vardas Matas");
-            Console.WriteLine(Console.ReadLine());
-            Console.WriteLine("įrašykite savo pirma vardo raidę ");
-            //Console.ReadKey();
-            //var vardas = Console.ReadLine();
-            Console.WriteLine("įvesta raidė: {0}", (int)Console.ReadKey().KeyChar);
-            Console.WriteLine("įvesta raidė: {0}", (int)Console.ReadKey().KeyChar);
+            Console.WriteLine("Mano vardas Matas"); //isvesti savo varda
+             Console.WriteLine(Console.ReadLine()); //nuskaitykite iš klaviaturos savo varda
+            Console.WriteLine("įrašykite savo pirma vardo raidę "); //isvestis i ekrana
+            Console.ReadKey();
+            var vardas = Console.ReadLine();
+            Console.WriteLine("įvestas ASCII: {0}", (int)Console.ReadKey().KeyChar); //kodel keyChar? 
 
+             Console.WriteLine("įveskite savo vardo pirmaja raide ir betkoki skaiciu");
+             Console.WriteLine("rezultatas yra: {0}", (int)Console.ReadKey().KeyChar + (int)Console.ReadKey().KeyChar); //nuskaitomos dvi raides, tuomet sudeda isveda rezultata
 
+            Console.WriteLine("L\na\nb\na\ns");
+            Console.WriteLine("L\ta\tb\ta\ts"); 
 
+            Console.WriteLine("(1) Pirkti \n (2) Parduoti \n (3) Likučiai");
+            Console.WriteLine(" \"pasirinkimas yra : {0} \"", Console.ReadKey().KeyChar); 
 
+            Console.WriteLine("\u0022Labas\u0022"); 
 
+            Console.WriteLine("parašykite savo vardą, o aš atspėsiu jūsų antrą raidę");
+            Console.WriteLine(Console.ReadLine()[1]); 
+
+            Console.WriteLine("parasykite savo varda, o as atspesiu skaiciu");
+            Console.WriteLine(Console.ReadLine().Length); 
+
+            Console.WriteLine("      {0}         {1}", Console.ReadLine(), Console.ReadLine()); 
+
+            Console.WriteLine("\n\n\n{0}" , Console.ReadLine()); */
+
+            Console.WriteLine(@"
+                            *******************
+                **                                     **
+             **                                           **
+             **         ()         ><       ()             **
+            **                                             **
+             **                                           **
+               **           ^             ^              **
+                **           ^           ^              **
+                   **         ^^^^^^^^^^^            **
+                      **                         **
+                        *************************".Replace("*", "\""));
 
 
         }
