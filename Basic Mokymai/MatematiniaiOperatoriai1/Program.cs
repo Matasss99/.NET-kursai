668d13d15e8491fff61027de137cd8fbf99fe61b
Matas Jonaitis <matas_jonaitis@codeacademylt.onmicrosoft.com>
2022-06-14 20:51:38 +0300

    matematiniai operatoriai

---
+++ Basic Mokymai/MatematiniaiOperatoriai1/Program.cs
@@ -0,0 +1,56 @@
+﻿
+Console.WriteLine("Hello, Priskyrimo operatoriai = += -= *= /=!");
+int skaicius;
+int kitasSkaicius = 20;
+int nelyginisSkaicius = 5;
+skaicius = 10;
+kitasSkaicius = skaicius;
+Console.WriteLine($"skaicius = {skaicius}, kitasSkaicius = {kitasSkaicius}");
+
+kitasSkaicius += nelyginisSkaicius; //kitasSkaicius = kitasSkaiciu + nelyginisSkaicius //tai analogas
+Console.WriteLine($"kitasSkaicius = {kitasSkaicius}");
+
+kitasSkaicius -= nelyginisSkaicius;
+Console.WriteLine($"kitasSkaicius = {kitasSkaicius}");
+
+kitasSkaicius *= nelyginisSkaicius;
+Console.WriteLine($"kitasSkaicius = {kitasSkaicius}");
+
+kitasSkaicius /= nelyginisSkaicius;
+Console.WriteLine($"kitasSkaicius = {kitasSkaicius}");
+
+kitasSkaicius = 21;
+
+kitasSkaicius /= nelyginisSkaicius;
+Console.WriteLine($"kitasSkaicius = {kitasSkaicius}");
+
+double doubleSkaicius = 21;
+doubleSkaicius /= nelyginisSkaicius;
+Console.WriteLine($"kitasSkaicius = {doubleSkaicius}");
+
+
+Console.WriteLine();
+Console.WriteLine("Hello, matematiniai operatoriai + - * / % ++ --");
+//sudetis
+int suma = skaicius + kitasSkaicius;
+Console.WriteLine("suma = skaicius + kitasSkaicius = {0}", suma);
+//atimtis
+int atimtis = skaicius - kitasSkaicius;
+Console.WriteLine("atimtis = skaicius - kitasSkaicius = {0}", atimtis);
+int daugyba = skaicius * kitasSkaicius;
+Console.WriteLine("daugyba = skaicius*kitasSkaicius = {0}", daugyba);
+double dalyba = (double)skaicius / kitasSkaicius;
+Console.WriteLine("dalyba = skaicius/kitasSkaicius = {0}", dalyba);
+
+
+int matematinisRezultatas = 1 + 2 - 3 + 4 + nelyginisSkaicius - skaicius;
+
+int dalybaSuLiekana = nelyginisSkaicius % 2;
+Console.WriteLine("dalybaSuLiekana = nelyginisSkaicius % 2 = {0}", dalybaSuLiekana);
+
+skaicius++;
+Console.WriteLine($"skaicius = {skaicius}");
+
+skaicius--;
+Console.WriteLine($"skaicius = {skaicius}");
+
--- Basic Mokymai/tipuKonversijos/Program.cs
+++ Basic Mokymai/tipuKonversijos/Program.cs
@@ -77,6 +77,9 @@
             int tekstasIntParsintas = int.Parse(tesktas);
 
 
+
+
+
         }
     }
 }
