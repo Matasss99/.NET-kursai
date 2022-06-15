26f4c86c40c7de052d2623c11eccc934a0e4b0a2
Matas Jonaitis <matas_jonaitis@codeacademylt.onmicrosoft.com>
2022-06-15 19:20:13 +0300

    uzduotys

--- Basic Mokymai/MatematiniaiOperatoriai1/Program.cs
+++ Basic Mokymai/MatematiniaiOperatoriai1/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Text;
+
 Console.WriteLine("Hello, Priskyrimo operatoriai = += -= *= /=!");
 int skaicius;
 int kitasSkaicius = 20;
@@ -53,4 +55,63 @@ Console.WriteLine($"skaicius = {skaicius}");
 
 skaicius--;
 Console.WriteLine($"skaicius = {skaicius}");
+
+
+double side1 = 5.5;
+double side2 = 3.25;
+double height = 4.6;
+double area = (side1 + side2) / 2 * height;
+
+double areaKazkasKito = (side1 * 2 + side2) / (2 * height); //skliaustelius galima naudoti pagal matematines taisykles
+
+Console.OutputEncoding = Encoding.UTF8;
+int nulis = 0;
+int int10 = 10;
+long long10 = 10;
+double double10 = 10;
+//Console.WriteLine($"int10/nulis = {int10/nulis}"); //luzta
+//Console.WriteLine($"long10/nulis = {long10 / nulis}"); //luzta
+Console.WriteLine($"double10/nulis = {double10 / nulis}"); //grazina begalybe - ty begalybes implemenacija
+double a = double.PositiveInfinity;
+Console.WriteLine($"a = {a}");
+
+
+double a1 = double.NaN; // NaN = not a number
+
+//Overflow and Underflow
+short s1 = 30_000;
+short s2 = 30_000;
+short s3 = (short)(s1 + s2);
+Console.WriteLine($"s3={s3}");
+//checked
+//{ 
+//   s3 = (short)(s1 + s2);
+//}
+
+//int pirmasSkaicius = Convert.ToInt32(Console.ReadLine());
+
+
+//užduotis
+
+//Console.WriteLine("ĮVESKITE 1 SKAICIU: ");
+//int pirmasSkaicius = int.Parse(Console.ReadLine());
+//Console.WriteLine("ĮVESKITE 2 SKAICIU: ");
+//int antrasSkaicius = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"{pirmasSkaicius} + {antrasSkaicius} = {pirmasSkaicius+antrasSkaicius}");
+//Console.WriteLine($"{pirmasSkaicius} + {antrasSkaicius} = {pirmasSkaicius + antrasSkaicius}");
+//Console.WriteLine($"{pirmasSkaicius} - {antrasSkaicius} = {pirmasSkaicius - antrasSkaicius}");
+//Console.WriteLine($"{pirmasSkaicius} * {antrasSkaicius} = {pirmasSkaicius * antrasSkaicius}");
+//Console.WriteLine($"{pirmasSkaicius} / {antrasSkaicius} = {(double)pirmasSkaicius / antrasSkaicius}");
+
+Console.WriteLine("parašykite 1 skaičių:");
+double pirmasSkaicius = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("parašykite 2 skaičių:");
+double antrasSkaicius = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("parašykite 3 skaičių:");
+double treciasSkaicius = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Skaiciu vidurkis: {pirmasSkaicius} + {antrasSkaicius} + {treciasSkaicius} / 3 = {(pirmasSkaicius + antrasSkaicius + treciasSkaicius) / 3}");
+
+
+
 
