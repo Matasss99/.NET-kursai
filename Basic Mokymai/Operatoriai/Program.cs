39d78eb078d9e06b477414405d30e30ca79966dd
Matas Jonaitis <matas_jonaitis@codeacademylt.onmicrosoft.com>
2022-06-17 00:08:39 +0300

    tower of hanoi

---
+++ Basic Mokymai/Operatoriai/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Hello, Operatoriai!");
+
+//Reliaciniai operatoriai == != > < >= <=
+/* var skaicius = 10;
+var nelyginisSkaicius = 5;
+var lyginisSkaicius = 10;
+
+Console.WriteLine(" == patikrina ar kintamieji yra lygus");
+Console.WriteLine($"{skaicius}=={lyginisSkaicius} yra {skaicius==lyginisSkaicius}");
+bool ar10YraLygu5 = skaicius == nelyginisSkaicius;
+Console.WriteLine($"{skaicius}=={nelyginisSkaicius} yra {ar10YraLygu5}");
+Console.WriteLine("-------------------------------------");
+Console.WriteLine("!= patikrina ar kintamieji yra nelygus");
+Console.WriteLine($"{skaicius}!={lyginisSkaicius} yra {skaicius != lyginisSkaicius}");
+Console.WriteLine($"{skaicius}!={nelyginisSkaicius} yra {skaicius != nelyginisSkaicius}");
+Console.WriteLine("-------------------------------------");
+Console.WriteLine("> patikrina ar kaireje esanti reiksme yra didesne uz desineje");
+Console.WriteLine($"{skaicius}>{lyginisSkaicius} yra {skaicius > lyginisSkaicius}"); */
+
+long didziausias = long.MaxValue;
+short maziausias = short.MaxValue;
+double Rezultatas = (double)didziausias / maziausias;
+Console.WriteLine($"{(double)didziausias / maziausias}");
+double rezultatas1 = (Rezultatas - long.MaxValue);
+Console.WriteLine($"iš rezultato atimkite maksimalu long skaiciu {rezultatas1}");
+Console.WriteLine($"ir pridekite maksimalu int skaiciu {rezultatas1 + int.MaxValue}");
--- Basic Mokymai/Tower Of Hanoi/Program.cs
+++ Basic Mokymai/Tower Of Hanoi/Program.cs
@@ -15,24 +15,194 @@
 
 
             Console.WriteLine(@"
- Eil.1 {0} {5} {9}
- Eil.2 {1} {6} {10}
- Eil.3 {2} {7} {11}  
- Eil.4 {3} {8} {12}
- Eil.5 {4}", eilute1Stulpelis1, eilute2Stulpelis1, 
-             eilute3Stulpelis1, eilute4Stulpelis1, 
-             eilute5Stulpelis1, eilute1Stulpelis1, 
-             eilute1Stulpelis1, eilute1Stulpelis1, 
-             eilute1Stulpelis1, eilute1Stulpelis1, 
-             eilute1Stulpelis1, eilute1Stulpelis1, 
-             eilute1Stulpelis1);
+ Eil.1 {0} {5} {10}
+ Eil.2 {1} {6} {11}
+ Eil.3 {2} {7} {12}  
+ Eil.4 {3} {8} {13}
+ Eil.5 {4} {9} {14}", 
+ eilute1Stulpelis1, eilute2Stulpelis1, 
+ eilute3Stulpelis1, eilute4Stulpelis1, 
+ eilute5Stulpelis1, eilute1Stulpelis1, 
+ eilute1Stulpelis1, eilute1Stulpelis1, 
+ eilute1Stulpelis1, eilute1Stulpelis1, 
+ eilute1Stulpelis1, eilute1Stulpelis1, 
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1);
             Console.WriteLine("    ----1stulp----2stulp----3stulp----");
             
             
             Console.WriteLine("----------Tęskite Toliau----------", Console.ReadKey());
 
 
-         
+            Console.WriteLine(@"
+ Eil.1 {0} {5} {10}
+ Eil.2 {1} {6} {11}
+ Eil.3 {2} {7} {12}  
+ Eil.4 {3} {8} {13}
+ Eil.5 {4} {9} {14}",
+ eilute5Stulpelis1, eilute4Stulpelis1,
+ eilute3Stulpelis1, eilute2Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1);
+            Console.WriteLine("    ----1stulp----2stulp----3stulp----");
+
+
+            Console.WriteLine("----------Tęskite Toliau----------", Console.ReadKey());
+
+            Console.WriteLine(@"
+ Eil.1 {0} {5} {10}
+ Eil.2 {1} {6} {11}
+ Eil.3 {2} {7} {12}  
+ Eil.4 {3} {8} {13}
+ Eil.5 {4} {9} {14}",
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1);
+            Console.WriteLine("    ----1stulp----2stulp----3stulp----");
+
+            Console.WriteLine("----------Tęskite Toliau----------", Console.ReadKey());
+
+            Console.WriteLine(@"
+ Eil.1 {0} {5} {10}
+ Eil.2 {1} {6} {11}
+ Eil.3 {2} {7} {12}  
+ Eil.4 {3} {8} {13}
+ Eil.5 {4} {9} {14}",
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute5Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute5Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute5Stulpelis1);
+            Console.WriteLine("    ----1stulp----2stulp----3stulp----");
+
+            Console.WriteLine("----------Tęskite Toliau----------", Console.ReadKey());
+
+            Console.WriteLine(@"
+ Eil.1 {0} {5} {10}
+ Eil.2 {1} {6} {11}
+ Eil.3 {2} {7} {12}  
+ Eil.4 {3} {8} {13}
+ Eil.5 {4} {9} {14}",
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute5Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute4Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute2Stulpelis1,
+ eilute3Stulpelis1);
+            Console.WriteLine("    ----1stulp----2stulp----3stulp----");
+
+            Console.WriteLine("----------Tęskite Toliau----------", Console.ReadKey());
+
+
+            Console.WriteLine(@"
+ Eil.1 {0} {5} {10}
+ Eil.2 {1} {6} {11}
+ Eil.3 {2} {7} {12}  
+ Eil.4 {3} {8} {13}
+ Eil.5 {4} {9} {14}",
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute2Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute2Stulpelis1,
+ eilute1Stulpelis1);
+            Console.WriteLine("    ----1stulp----2stulp----3stulp----");
+
+            Console.WriteLine("----------Tęskite Toliau----------", Console.ReadKey());
+
+            Console.WriteLine(@"
+ Eil.1 {0} {5} {10}
+ Eil.2 {1} {6} {11}
+ Eil.3 {2} {7} {12}  
+ Eil.4 {3} {8} {13}
+ Eil.5 {4} {9} {14}",
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute3Stulpelis1,
+ eilute3Stulpelis1, eilute3Stulpelis1,
+ eilute3Stulpelis1, eilute3Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute3Stulpelis1);
+            Console.WriteLine("    ----1stulp----2stulp----3stulp----");
+
+            Console.WriteLine("----------Tęskite Toliau----------", Console.ReadKey());
+
+            Console.WriteLine(@"
+ Eil.1 {0} {5} {10}
+ Eil.2 {1} {6} {11}
+ Eil.3 {2} {7} {12}  
+ Eil.4 {3} {8} {13}
+ Eil.5 {4} {9} {14}",
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute2Stulpelis1,
+ eilute3Stulpelis1, eilute4Stulpelis1,
+ eilute5Stulpelis1);
+            Console.WriteLine("    ----1stulp----2stulp----3stulp----");
+
+            Console.WriteLine("----------Tęskite Toliau----------", Console.ReadKey());
+
+            
+            
+            Console.WriteLine(@"
+ Eil.1 {0} {5} {10}
+ Eil.2 {1} {6} {11}
+ Eil.3 {2} {7} {12}  
+ Eil.4 {3} {8} {13}
+ Eil.5 {4} {9} {14}",
+ eilute1Stulpelis1.Replace("#", "\""), eilute2Stulpelis1.Replace("#", "\""),
+ eilute3Stulpelis1.Replace("#", "\""), eilute4Stulpelis1.Replace("#", "\""),
+ eilute5Stulpelis1.Replace("#", "\""), eilute1Stulpelis1,
+ eilute2Stulpelis1.Replace("#", "\""), eilute3Stulpelis1.Replace("#", "\""),
+ eilute4Stulpelis1.Replace("#", "\""), eilute5Stulpelis1.Replace("#", "\""),
+ eilute1Stulpelis1, eilute2Stulpelis1.Replace("#", "\""),
+ eilute3Stulpelis1.Replace("#", "\""), eilute4Stulpelis1.Replace("#", "\""),
+ eilute5Stulpelis1.Replace("#", "\""));
+            Console.WriteLine("    ----1stulp----2stulp----3stulp----");
+
+            Console.WriteLine("----------Tęskite Toliau----------", Console.ReadKey());
+
+            Console.WriteLine("Nupieškite 1stulpelio 1eilutę");
+          
+            Console.WriteLine(@"
+ Eil.1    {0}    {5} {10}
+ Eil.2 {1} {6} {11}
+ Eil.3 {2} {7} {12}  
+ Eil.4 {3} {8} {13}
+ Eil.5 {4} {9} {14}",
+ Console.ReadLine(), eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1, eilute1Stulpelis1,
+ eilute1Stulpelis1);
+            Console.WriteLine("    ----1stulp----2stulp----3stulp----");
+
+
+
+
         }
     }
 }
