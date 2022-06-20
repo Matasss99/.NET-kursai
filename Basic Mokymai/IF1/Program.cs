4941925588310cf2c8e222dc85c314266cecfce2
Matas Jonaitis <matas_jonaitis@codeacademylt.onmicrosoft.com>
2022-06-20 21:59:55 +0300

    IF

--- Basic Mokymai/IF1/Program.cs
+++ Basic Mokymai/IF1/Program.cs
@@ -92,19 +92,76 @@
                 Console.WriteLine("Jus dar turite galimybiu");
             }
 
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("null-coalescing operator (??)");
+            bool? nullableBool = true;
+            bool normalBool;
+            //normalBool = nullableBool; // taip negalima
+            normalBool = nullableBool ?? false;
+            //--------------------
+            nullableBool ??= false;
 
 
+            /*   // uzduotis NR1
+               Console.WriteLine("Iveskite skaiciu: ");
+               var skaicius = Convert.ToInt32(Console.ReadLine());
+               if (skaicius % 2 == 0)
+               {
+                   Console.WriteLine("Skaicius yra lyginis");
+               }
+               if (skaicius < 0)
+               {
+                   Console.WriteLine("skaicius yra neigiamas");
+               }
+               if (skaicius % 2 != 0 && skaicius > 0)
+               {
+                   Console.WriteLine("Skaicius " + skaicius  );
+               } 
 
+            // uzduotis NR2
+            Console.WriteLine("Iveskite grupes nariu skaiciu: ");
+            var nariuSkaicius = Convert.ToInt32(Console.ReadLine());
+            if (nariuSkaicius == 1)
+            {
+                Console.WriteLine("Tai solo atlikejas");
+            }
+            else if (nariuSkaicius == 2)
+            {
+                Console.WriteLine("Tai duetas");
+            }
+            else if (nariuSkaicius > 2 && nariuSkaicius < 10  )
+            {
+                Console.WriteLine("Tai ansamblis");
+                
+            }
+            else if (nariuSkaicius >= 10)
+            {
+                Console.WriteLine("tai kamerinis ansamblis");
+            }
+            else
+            {
+                Console.WriteLine("Klaida");
+            } */
 
-
-
-
-
-
-
-
-
-
+            // uzduotis NR3
+            Console.WriteLine("Iveskite isdirbtas valandas: ");
+            bool arGerasSkaicius = int.TryParse(Console.ReadLine(), out int input);
+            if (input < 160 && arGerasSkaicius)
+            {
+                Console.WriteLine($"Dar reikia isdirbti {160 - input}");
+            }
+            else if (input == 160)
+            {
+                Console.WriteLine("Isdirbtas pilnas etatas");
+            }
+            else if (input > 160)
+            {
+                Console.WriteLine($"virsvalandziu yra : {input - 160}");
+            }
+            else
+            {
+                Console.WriteLine("Klaida");
+            }
 
 
 
